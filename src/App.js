6874ac765e7da19e3a3f912f6fa346197ed5626f
Mandy Arens <40013860+maanderz@users.noreply.github.com>
2020-04-07 16:40:16 -0600

    wrote dice roll function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [diceSum, setDiceSum] = useState(0);
+
+  const diceRoll = numberOfDice => {
+    let rolls = [];
+    let rollSum = 0;
+
+    for (let i = 0; i < numberOfDice; i++) {
+      // add the 1 so the die number is not 0
+      rolls[i] = Math.floor(Math.random() * 6) + 1;
+      console.log(rolls[i]);
+      rollSum += rolls[i];
+    }
+    setDiceSum(rollSum);
+  };
+
+  console.log("here", diceSum);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Dice Roll Demo</h1>
+      <button onClick={() => diceRoll(3)}> Click me </button>
     </div>
   );
 }
